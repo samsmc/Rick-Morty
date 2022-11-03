7cf408022f573b142714f6abc23c0f6c441f042b
SamanthaMartins <samantha.martins@daysk.com>
2022-11-03 12:18:52 +0100

    removed routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ErrorPage from './components/error_page/ErrorPage';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import Homepage from './pages/homepage';
@@ -50,6 +48,8 @@ const App = () => {
 	const [info, setInfo] = useState<Info>();
 	const [searchTerm, setSearchTerm] = useState<string>('');
 
+	console.log('Welcome to my code!')
+
 	const fetchUrlPage = (url?: string) => {
 		fetchUrl(url).then((data: Data) => {
 			setCharacters(data.results);
@@ -93,28 +93,18 @@ const App = () => {
 
 	return (
 		<>
-			<BrowserRouter>
-				<Navbar />
-				<Routes>
-					<Route
-						path="/"
-						element={
-							<Homepage
-								characters={characters}
-								type={type}
-								setType={setType}
-								setSearchTerm={setSearchTerm}
-								info={info}
-								fetchNextPage={fetchNextPage}
-								fetchPreviousPage={fetchPreviousPage}
-							/>
-						}
-					/>
-					<Route path="*" element={<ErrorPage />} />
-				</Routes>
-				<Pagination info={info} fetchNextPage={fetchNextPage} fetchPreviousPage={fetchPreviousPage} />
-				<Footer />
-			</BrowserRouter>
+			<Navbar />
+			<Homepage
+				characters={characters}
+				type={type}
+				setType={setType}
+				setSearchTerm={setSearchTerm}
+				info={info}
+				fetchNextPage={fetchNextPage}
+				fetchPreviousPage={fetchPreviousPage}
+			/>
+			<Pagination info={info} fetchNextPage={fetchNextPage} fetchPreviousPage={fetchPreviousPage} />
+			<Footer />
 		</>
 	);
 };
